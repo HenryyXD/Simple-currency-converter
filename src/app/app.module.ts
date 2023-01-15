94d215a4048066378eccb4aaf516ff96738a6319
HenryyXD <rickloubaque@gmail.com>
2023-01-15 18:01:14 -0300

    inicia mini conversor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ConversorComponent } from './conversor/conversor.component';
-import { SelecionadorMoedaComponent } from './conversor/selecionador-moeda/selecionador-moeda.component';
 import { MiniConversorComponent } from './conversor/mini-conversor/mini-conversor.component';
+import { SelecionadorMoedaComponent } from './conversor/selecionador-moeda/selecionador-moeda.component';
 
 @NgModule({
-  declarations: [AppComponent, ConversorComponent, SelecionadorMoedaComponent, MiniConversorComponent],
+  declarations: [
+    AppComponent,
+    ConversorComponent,
+    SelecionadorMoedaComponent,
+    MiniConversorComponent,
+  ],
   imports: [
     BrowserModule,
     MatToolbarModule,
@@ -27,7 +33,9 @@ import { MiniConversorComponent } from './conversor/mini-conversor/mini-converso
     BrowserAnimationsModule,
     MatIconModule,
     HttpClientModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/conversor/mini-conversor/mini-conversor.component.ts
+++ src/app/conversor/mini-conversor/mini-conversor.component.ts
@@ -1,10 +1,26 @@
 import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { of } from 'rxjs';
+import { ConversorService } from '../conversor.service';
+import moedasJson from './../../../assets/currencies.json';
 
 @Component({
   selector: 'mini-conversor',
   templateUrl: './mini-conversor.component.html',
-  styleUrls: ['./mini-conversor.component.scss']
+  styleUrls: ['./mini-conversor.component.scss'],
 })
 export class MiniConversorComponent {
-  @Input() moedaSelecionada: string = '';
+  formControl = new FormControl('');
+  moedasSelecionadas: string[] = [];
+  @Input() moedaFrom: string = 'USD';
+  moedas = moedasJson;
+  objectkeys = Object.keys;
+
+  constructor(private conversorService: ConversorService) {
+  }
+
+
+  moedaSelecionadaChange({ value }: any) {
+    this.moedasSelecionadas = value;
+  }
 }
