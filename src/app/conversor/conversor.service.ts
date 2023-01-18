8bd96d9a1f2c265147318e0292e20bd0b75d3848
HenryyXD <rickloubaque@gmail.com>
2023-01-18 17:55:14 -0300

    adiciona conversao aproximada

--- src/app/conversor/conversor.service.ts
+++ src/app/conversor/conversor.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import conversionsJSON from './../../assets/conversions.json';
-import { map, Observable, of } from 'rxjs';
+import { map, Observable, of, forkJoin, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -12,20 +12,29 @@ export class ConversorService {
   private conversions: { [key: string]: string } = conversionsJSON;
   constructor(private http: HttpClient) {}
 
+  private moedasCache: { [key: string]: {} } = {};
+
   getConversao(moedaFrom: string, moedaTo: string): Observable<any> {
     if (moedaFrom === moedaTo) {
       return of({
-        bid: 1,
+        bid: 1
       });
     }
 
     let nome = this.procurarNomeConversao(moedaFrom, moedaTo);
-    if (!nome) return this.getConversaoAproximada();
+    if (!nome && moedaFrom !== 'USD' && moedaTo !== 'USD') {
+      return this.getConversaoAproximada(moedaFrom, moedaTo);
+    } else if(!nome) {
+      return of([]); // nao existe conversao
+    }
 
     let requestUrl = `${this.API}/json/last/${nome}`;
     return this.http
       .get(requestUrl)
-      .pipe(map((res: any) => res[Object.keys(res)[0]]));
+      .pipe(
+        tap((res: any) => console.log(res)),
+        map((res: any) => res[Object.keys(res)[0]])
+      );
   }
 
   procurarNomeConversao(moedaFrom: string, moedaTo: string): string {
@@ -45,24 +54,34 @@ export class ConversorService {
     return nome;
   }
 
-  getConversaoAproximada() {
-    return of([]);
+  // A fórmula para calcular a taxa cruzada entre duas moedas é:
+
+  // Taxa cruzada = (Taxa de câmbio A/B) x (Taxa de câmbio B/C)
+
+  // onde A, B e C são as moedas envolvidas na conversão e "Taxa de câmbio A/B" é a taxa de
+  //câmbio entre as moedas A e B, e "Taxa de câmbio B/C" é a taxa de câmbio entre as moedas B e C.
+
+  // Por exemplo, se você quiser calcular a taxa cruzada entre o dólar americano (USD) e o
+  //iene japonês (JPY) e você tem acesso as taxas de câmbio entre o dólar americano e o
+  //euro (EUR) e entre o euro e o iene japonês, você pode calcular a taxa cruzada como:
+
+  // Taxa cruzada USD/JPY = (Taxa de câmbio USD/EUR) x (Taxa de câmbio EUR/JPY)
+
+  // É importante notar que a taxa cruzada não é necessariamente igual a taxa de câmbio direta
+  //ou indireta entre as moedas A e C, dependendo das taxas de câmbio entre as moedas B e C, pode
+  //haver uma diferença significativa.
+  //Fonte: ChatGPT :)
+  getConversaoAproximada(moedaFrom: string, moedaTo: string) {
+    let taxaA = this.getConversao(moedaFrom, 'USD');
+    let taxaB = this.getConversao(moedaTo, 'USD');
+
+    return forkJoin([taxaA, taxaB]).pipe(
+      map((r: any) => {
+        let responseTaxaA = r[0];
+        let responseTaxaB = r[1];
+        let taxaCruzada = responseTaxaB.bid / responseTaxaA.bid;
+        return { bid: taxaCruzada };
+      })
+    );
   }
 }
-
-// A fórmula para calcular a taxa cruzada entre duas moedas é:
-
-// Taxa cruzada = (Taxa de câmbio A/B) x (Taxa de câmbio B/C)
-
-// onde A, B e C são as moedas envolvidas na conversão e "Taxa de câmbio A/B" é a taxa de
-//câmbio entre as moedas A e B, e "Taxa de câmbio B/C" é a taxa de câmbio entre as moedas B e C.
-
-// Por exemplo, se você quiser calcular a taxa cruzada entre o dólar americano (USD) e o
-//iene japonês (JPY) e você tem acesso as taxas de câmbio entre o dólar americano e o
-//euro (EUR) e entre o euro e o iene japonês, você pode calcular a taxa cruzada como:
-
-// Taxa cruzada USD/JPY = (Taxa de câmbio USD/EUR) x (Taxa de câmbio EUR/JPY)
-
-// É importante notar que a taxa cruzada não é necessariamente igual a taxa de câmbio direta
-//ou indireta entre as moedas A e C, dependendo das taxas de câmbio entre as moedas B e C, pode
-//haver uma diferença significativa.
